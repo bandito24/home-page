bbd812d039e10735c9e7c8daab7e15ee0d7e1330
Charlie Mudd <cmudd6@gmail.com>
2023-03-16 23:46:38 -0700

    pages all linked together

--- home.php
+++ home.php
@@ -1,3 +1,129 @@
+<script type="text/javascript" src="../../scripts/jquery-3.4.1.min.js"></script>
+<?php
+include('../../init.php');
+
+include(DIR_LIBRARIES.'/cls.species.php');
+
+$Species = new Species();
+
+$optionsAll = array(
+    'where' => array(
+        "plant_type LIKE '%%'",
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsTree = array(
+    'where' => array(
+        "plant_type LIKE '%tree%'",
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsShrub = array(
+    'where' => array(
+        "plant_type LIKE '%shrub%'",
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsPHerb = array(
+    'where' => array(
+        "plant_type LIKE '%herb%'",
+        "duration LIKE '%perennial%'",
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsAHerb = array(
+    'where' => array(
+        "plant_type LIKE '%herb%'",
+        "duration LIKE '%annual%'",
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsVine = array(
+    'where' => array(
+        "plant_type LIKE '%vine%'",
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsFern = array(
+    'where' => array(
+        "plant_type LIKE '%fern%'",
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsGrass = array(
+    'where' => array(
+        "plant_type LIKE '%grass%'",
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsSucculent = array(
+    'where' => array(
+        "plant_type LIKE '%succulent%'",
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsBulb = array(
+    'where' => array(
+        "plant_type LIKE '%bulb%'",
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsLowW = array(
+    'where' => array(
+        "water LIKE '%low%'",
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsSun = array(
+    'where' => array(
+        "sun LIKE '%sun%'",
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsPShade = array(
+    'where' => array(
+        "sun LIKE '%part%'",
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsShade = array(
+    'where' => array(
+        "sun IN ('Shade|Part Shade', 'Part Shade|Shade', 'Shade', 'Sun|Shade', 'Shade|Sun', 'Sun|Part Shade|Shade', 'Sun|Shade|Part Shade', 'Part Shade|Sun|Shade', 'Part Shade|Shade|Sun', 'Shade|Sun|Part Shade', 'Shade|Part Shade|Sun')"
+    ),
+    'rows_per_page' => 1, 
+); 
+$optionsEvergreen = array(
+    'where' => array(
+        "leaf_retention LIKE '%evergreen%'"
+    ),
+    'rows_per_page' => 1, 
+); 
+
+// $options1 =
+
+$allCount = $Species->ofCategory('all_plants', $optionsAll);
+$treeCount = $Species->ofCategory('all_plants', $optionsTree);
+$shrubCount = $Species->ofCategory('all_plants', $optionsShrub);
+$pHerbCount = $Species->ofCategory('all_plants', $optionsPHerb);
+$aHerbCount = $Species->ofCategory('all_plants', $optionsAHerb);
+$vineCount = $Species->ofCategory('all_plants', $optionsVine);
+$fernCount = $Species->ofCategory('all_plants', $optionsFern);
+$grassCount = $Species->ofCategory('all_plants', $optionsGrass);
+$succulentCount = $Species->ofCategory('all_plants', $optionsSucculent);
+$bulbCount = $Species->ofCategory('all_plants', $optionsBulb);
+$lowWCount = $Species->ofCategory('all_plants', $optionsLowW);
+$sunCount = $Species->ofCategory('all_plants', $optionsSun);
+$pShadeCount = $Species->ofCategory('all_plants', $optionsPShade);
+$shadeCount = $Species->ofCategory('all_plants', $optionsShade);
+$evergreenCount = $Species->ofCategory('all_plants', $optionsEvergreen);
+
+$data = $pageData['rows'];
+
+echo ("<pre>");
+print_r($pageData);
+echo ("</pre>");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -74,69 +200,68 @@
         <div id="grid-container">
             <div class="grid-plant">
                 <a href="../static/index.php?cat=all_plants&page=1&sort=ranking"><img class="grid-image" src="./test-photos/all-plants.jpeg" alt="All Plants"></a> 
-                <p class="grid-text">All Plants (8090)</p>
+                <p class="grid-text">All Plants (<?php echo $allCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
                 <a href="../static/index.php?cat=tree&page=1&sort=ranking"><img class="grid-image" src="./test-photos/trees.jpeg" alt="Trees"></a>
-                <p class="grid-text">Trees (1704)</p>
+                <p class="grid-text">Trees (<?php echo $treeCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
                 <a href="../static/index.php?cat=shrub&page=1&sort=ranking"><img class="grid-image" src="./test-photos/shrubs.jpeg" alt="Shrubs"></a>
-                <p class="grid-text">Shrubs (2905)</p>
+                <p class="grid-text">Shrubs (<?php echo $shrubCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
-                <img class="grid-image" src="./test-photos/perennials.jpeg" alt="Perennial Herbs">
-                <p class="grid-text">Perennial Herbs (2732)</p>
+                <a href="../static/index.php?cat=herb&duration=perennial&page=1&sort=ranking"><img class="grid-image" src="./test-photos/perennials.jpeg" alt="Perennial Herbs"></a>
+                <p class="grid-text">Perennial Herbs (<?php echo $pHerbCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
-                <img class="grid-image" src="./test-photos/annuals.jpeg" alt="Annual Herbs">
-                <p class="grid-text">Annual Herbs (389)</p>
+                <a href="../static/index.php?cat=herb&duration=annual&page=1&sort=ranking"><img class="grid-image" src="./test-photos/annuals.jpeg" alt="Annual Herbs"></a>
+                <p class="grid-text">Annual Herbs (<?php echo $aHerbCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
                 <a href="../static/index.php?cat=vine&page=1&sort=ranking"><img class="grid-image" src="./test-photos/vines.jpeg" alt="Vines"></a>
-                <p class="grid-text">Vines (368)</p>
+                <p class="grid-text">Vines (<?php echo $vineCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
                 <a href="../static/index.php?cat=fern&page=1&sort=ranking"><img class="grid-image" src="./test-photos/ferns.jpeg" alt="Ferns"></a>
-                <p class="grid-text">Ferns (150)</p>
+                <p class="grid-text">Ferns (<?php echo $fernCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
                 <a href="../static/index.php?cat=grass&page=1&sort=ranking"><img class="grid-image" src="./test-photos/grasses.jpeg" alt="Grasses"></a>
-                <p class="grid-text">Grasses (572)</p>
+                <p class="grid-text">Grasses (<?php echo $grassCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
                 <a href="../static/index.php?cat=succulent&page=1&sort=ranking"><img class="grid-image" src="./test-photos/succulents.jpeg" alt="Succulents"></a>
-                <p class="grid-text">Succulents (419)</p>
+                <p class="grid-text">Succulents (<?php echo $succulentCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
                 <a href="../static/index.php?cat=bulb&page=1&sort=ranking"><img class="grid-image" src="./test-photos/bulbs.jpeg" alt="Bulbs"></a>
-                <p class="grid-text">Bulbs (392)</p>
+                <p class="grid-text">Bulbs (<?php echo $bulbCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
-                <img class="grid-image" src="./test-photos/low-water.jpeg" alt="Low Water Plants">
-                <p class="grid-text">Low Water (3487)</p>
+                <a href="../static/index.php?cat=all_plants&water=low&page=1&sort=ranking"><img class="grid-image" src="./test-photos/low-water.jpeg" alt="Low Water Plants"></a>
+                <p class="grid-text">Low Water (<?php echo $lowWCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
-                <img class="grid-image" src="./test-photos/full-sun.jpeg" alt="Full Sun Plants">
-                <p class="grid-text">Full Sun (3231)</p>
+                <a href="../static/index.php?cat=all_plants&sun=sun&page=1&sort=ranking"><img class="grid-image" src="./test-photos/full-sun.jpeg" alt="Full Sun Plants"></a>
+                <p class="grid-text">Full Sun (<?php echo $sunCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
-                <img class="grid-image" src="./test-photos/part-shade.jpeg" alt="Part Shade Plants">
-                <p class="grid-text">Part Shade (4873)</p>
+                <a href="../static/index.php?cat=all_plants&sun=part&page=1&sort=ranking"><img class="grid-image" src="./test-photos/part-shade.jpeg" alt="Part Shade Plants"></a>
+                <p class="grid-text">Part Shade (<?php echo $pShadeCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
-                <img class="grid-image" src="./test-photos/full-shade.jpeg" alt="Full Shade Plants">
-                <p class="grid-text">Full Shade (1521)</p>
+                <a href="../static/index.php?cat=all_plants&sun=shade&page=1&sort=ranking"><img class="grid-image" src="./test-photos/full-shade.jpeg" alt="Full Shade Plants"></a>
+                <p class="grid-text">Full Shade (<?php echo $shadeCount['total_rows']; ?>)</p>
             </div>
             <div class="grid-plant">
-                <img class="grid-image" src="./test-photos/evergreens.jpeg" alt="Evergreen Plants">
-                <p class="grid-text">Evergreens (4312)</p>
+                 <a href="../static/index.php?cat=all_plants&leaf_retention=evergreen&page=1&sort=ranking"><img class="grid-image" src="./test-photos/evergreens.jpeg" alt="Evergreen Plants"></a>
+                <p class="grid-text">Evergreens (<?php echo $evergreenCount['total_rows']; ?>)</p>
             </div>
         </div>
 
 
     </main>
-     
     <footer>
             <div id="plantlist-library">
                 <img class='footer-icon' src="./icons/plantlist-library.png" alt="Plantlist Library icon">
@@ -155,7 +280,9 @@
         </div>
             <p id="copyright">&copy 2019</p>
     </footer>
+    
 <script src="./index.js"></script>
 </div>
+
 </body>
 </html>
